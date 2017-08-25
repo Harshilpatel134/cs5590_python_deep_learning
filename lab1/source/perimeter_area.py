c76c0ceb0baf4887147762c8664b21574e940bb1
Harshilpatel134 <Harshilpatel007@live.com>
2017-08-25 17:05:32 -0500

    lab1

---
+++ lab1/source/perimeter_area.py
@@ -0,0 +1,8 @@
+import math
+rs=input("enter radius:")
+area=math.pi*int(rs)*int(rs)
+perimeter=2*math.pi*int(rs)
+print("area: %f" %area + " perimeter: %f" %perimeter)
+
+
+
---
+++ lab1/source/reverse.py
@@ -0,0 +1,3 @@
+n1=input("enter the no.:")
+n1=n1[::-1]
+print("ans:%d" %int(n1))
